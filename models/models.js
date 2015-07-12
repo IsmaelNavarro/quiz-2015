2ee34d9cdbf48342b71fa4b89987ba7f15e3461e
Ismael Navarro <ismanapa@gmail.com>
2015-07-12 16:41:19 +0200

    Cambios en la importacion de quiz

--- models/models.js
+++ models/models.js
@@ -26,8 +26,9 @@ var sequelize = new Sequelize(DB_name, user, pwd,
   }      
 );
 
-//Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname, "quiz"));
+// Importar definicion de la tabla Quiz
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz; //exportar definicion de tabla Quiz
 
