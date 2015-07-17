3b8f60f6bca8e95fe9ce1e03e2af642f10c6de16
Ismael Navarro <ismanapa@gmail.com>
2015-07-17 21:44:58 +0200

    Añadida creacion de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -42,3 +42,22 @@ exports.answer = function(req, res) {
 
 	res.render("quizes/answer", {quiz: req.quiz, respuesta: resultado});
 };
+
+//GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build(
+			{pregunta: "", respuesta: ""}
+		);
+
+	res.render('quizes/new', {quiz: quiz});
+};
+
+//POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	//Guardar en BD los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
+		res.redirect("/quizes");
+	});
+};
